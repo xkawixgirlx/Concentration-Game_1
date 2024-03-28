a8c8e8dce79288f568c8d803ca5efc9f0158250c
xkawixgirlx <jennkosh@gmail.com>
2024-03-28 13:53:46 -0700

    Add code section headers

--- main.js
+++ main.js
@@ -1,13 +1,46 @@
   /*----- constants -----*/
 
 
+
+
   /*----- state variables -----*/
+  /*States:
+  Flipped/not flipped
+  1st card? 
+  Board render
+  Audio
+  Play Again/Play Button 
+  Timer Countdown
+  Modes/Levels*/
+
 
 
   /*----- cached elements  -----*/
 
+//!
+//*
+//?
+
 
   /*----- event listeners -----*/
+.addEventListener(evt);
+
 
 
   /*----- functions -----*/
+init ();
+
+/*Pseudocode:
+define win condition/Lose Condition - if else? maybe while if Zen Mode
+define array of cards x2
+Event listeners for Audio and play again/ first card flip buttons
+Render cards();
+RenderBoard();
+Hard Mode - adjusts the timer time 
+Zen Mode - no time limit Hard Code end or pause button. 
+Logic to know if cards are matching. 
+Does cards from Array1 === Array2 if true increment score? 
+All cards match === win
+Timer hits Zero === Loss
+Zen Mode: how many points can you get. winner = null;
+ */
